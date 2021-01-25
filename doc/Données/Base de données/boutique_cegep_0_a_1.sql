1883757728062bac5c8fef0c7833fa634f0fab42
Simon Delarue <51883276+simondelarue14@users.noreply.github.com>
2021-01-25 08:31:59 -0500

    Changement des maquettes #2
    Changement de l'entête des maquettes

--- doc/Données/Base de données/boutique_cegep_0_a_1.sql
+++ doc/Données/Base de données/boutique_cegep_0_a_1.sql
@@ -26,7 +26,7 @@ SET row_security = off;
 CREATE DATABASE boutiquecegep WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
 
 
-ALTER DATABASE boutiquecegep OWNER TO "Groupe Boutique";
+ALTER DATABASE boutiquecegep OWNER TO postgres;
 
 \connect boutiquecegep
 
@@ -59,11 +59,11 @@ CREATE TABLE public.produit (
 );
 
 
-ALTER TABLE public.produit OWNER TO kenny;
+ALTER TABLE public.produit OWNER TO postgres;
 
 --
 -- TOC entry 202 (class 1259 OID 16390)
--- Name: produit_id_seq; Type: SEQUENCE; Schema: public; Owner: kenny
+-- Name: produit_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE public.produit_id_seq
@@ -75,12 +75,12 @@ CREATE SEQUENCE public.produit_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.produit_id_seq OWNER TO kenny;
+ALTER TABLE public.produit_id_seq OWNER TO postgres;
 
 --
 -- TOC entry 2934 (class 0 OID 0)
 -- Dependencies: 202
--- Name: produit_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: kenny
+-- Name: produit_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE public.produit_id_seq OWNED BY public.produit.id;
@@ -88,7 +88,7 @@ ALTER SEQUENCE public.produit_id_seq OWNED BY public.produit.id;
 
 --
 -- TOC entry 2797 (class 2604 OID 16395)
--- Name: produit id; Type: DEFAULT; Schema: public; Owner: kenny
+-- Name: produit id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.produit ALTER COLUMN id SET DEFAULT nextval('public.produit_id_seq'::regclass);
@@ -97,7 +97,7 @@ ALTER TABLE ONLY public.produit ALTER COLUMN id SET DEFAULT nextval('public.prod
 --
 -- TOC entry 2927 (class 0 OID 16392)
 -- Dependencies: 203
--- Data for Name: produit; Type: TABLE DATA; Schema: public; Owner: kenny
+-- Data for Name: produit; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 INSERT INTO public.produit VALUES (1, 'T-Shrit des Capitaines', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 34.99, 'null');
@@ -115,7 +115,7 @@ INSERT INTO public.produit VALUES (6, 'Moniteur', '"T-Shrit des Capitaines"
 --
 -- TOC entry 2935 (class 0 OID 0)
 -- Dependencies: 202
--- Name: produit_id_seq; Type: SEQUENCE SET; Schema: public; Owner: kenny
+-- Name: produit_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.produit_id_seq', 1, false);
@@ -123,7 +123,7 @@ SELECT pg_catalog.setval('public.produit_id_seq', 1, false);
 
 --
 -- TOC entry 2799 (class 2606 OID 16400)
--- Name: produit produit_pkey; Type: CONSTRAINT; Schema: public; Owner: kenny
+-- Name: produit produit_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.produit
