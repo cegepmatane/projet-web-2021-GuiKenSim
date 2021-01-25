1747fc37c96ca4b3504bb0409d00cbd3ed5c3393
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2021-01-25 08:18:29 -0500

    POC de la connexion la BDD #2 #3
    Début du DAO

---
+++ poc/Connexion à la base de donnée/listerProduits.php
@@ -0,0 +1,31 @@
+<?php
+require "./accesseur/ProduitDAO.php";
+$listeProduits = ProduitDAO::listerProduits();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        foreach($listeProduits as $produit)
+        {
+            ?>
+                <div>
+                    <h3 class="titre"><?=$produit->titre;?></h3>
+                    <spam><?=$produit->prix;?>$</span>
+                    <p><?=$produit->description;?></p>
+                    <spam><?=$produit->image;?></span>
+                </div>
+            <?php
+        }
+    ?>
+
+</body>
+</html>
