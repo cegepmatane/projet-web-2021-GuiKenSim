006a2e023fea8664b6606369101269e4f960566a
guillaumedalbignac <dalbignacguillaume@gmail.com>
2021-01-25 10:19:25 -0500

    Ajout page à propos sans css #1
    Ajout poc page à propos sans css

---
+++ poc/Page À propos sans CSS/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> Boutique du Cégep de Matane </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<header class="menu">
+    	<div class="logo">    
+	        <a href="index.php"><img src="./logo_cegep.png" alt="Logo Cégep Matane" class="logo" /></a>      
+    	</div>
+    	<div class="lien_administration">
+	         <a href="administration.php"> Administration </a>
+    	</div>
+  	</header>
+
+  	<div class="accueil">
+  		<h1> Bienvenue sur la boutique en ligne du Cégep de Matane ! Cliquez sur MAGASINER pour commencer. </h1>
+  		<a href="magasiner.php" class="bouton_magasiner"> MAGASINER </a>
+	  		<div class="lien_apropos">
+	  			<a href="apropos.php"> À Propos de nous </a>
+	  		</div> 		
+  	</div> 
+
+  	<footer class="copyright">
+  		<div>
+  			<p> © Tous droits réservés - Site développé par Kenny Maréchal, Simon Delarue et Guillaume d'Albignac </p>
+  		</div>  		
+  	</footer>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
