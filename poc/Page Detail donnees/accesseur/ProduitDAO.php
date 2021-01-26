889463ecb14d9a2259a7b9bcce246a297b084e3d
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2021-01-26 12:56:27 -0500

    page detail produit #2 #4
    Update ProduitDAO.php avec la méthode listerProduitParId

--- poc/Page Detail donnees/accesseur/ProduitDAO.php
+++ poc/Page Detail donnees/accesseur/ProduitDAO.php
@@ -12,5 +12,16 @@ class ProduitDAO{
 
         return $listeProduits;
     }
+
+    public static function listerProduitParId($id){
+        $MESSAGE_SQL_LISTER_PRODUIT_PAR_ID = "SELECT id, titre, description, prix, image FROM produit WHERE id=".$id.";";
+        $baseDeDonnees = BaseDeDonnees::getConnexion();
+        $requetteListerProduitsParId = $baseDeDonnees->prepare($MESSAGE_SQL_LISTER_PRODUIT_PAR_ID);
+        //$requetteListerProduitsParId->bindParam(':id', $id, PDO::PARAM_INT);
+        $requetteListerProduitsParId->execute();
+    
+        $produit = $requetteListerProduitsParId->fetch();
+        return $produit;
+    }
 }
 ?>
---
+++ poc/Page Detail donnees/detaillerProduit.php
@@ -0,0 +1,31 @@
+<?php
+require "./accesseur/ProduitDAO.php";
+$produit = ProduitDAO::listerProduitParId(5);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title> Boutique du Cégep de Matane </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./Decoration/detaillerProduit.css">
+</head>
+<body>
+
+	<?php include "menu.php"?>
+
+  	<div class="contenu">
+		<div class="contenu-image">
+			<img src="https://place-hold.it/300x500" alt="#">
+		</div>
+		<div class="contenu-detailler">
+			<h1><?=$produit["titre"];?></h1>
+			<h3>Déscriptif du produit</h3>
+			<p><?=$produit["description"];?></p>
+			<span>Prix : <?=$produit["prix"];?>$</span>
+			<button class="bouton-acheter-produit">Acheter le produit</button>
+		</div>
+  	</div> 
+
+</body>
+</html>
