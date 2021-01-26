e0d3667cbc248504fafb72842d2ad6cf36301319
guillaumedalbignac <dalbignacguillaume@gmail.com>
2021-01-26 13:31:28 -0500

    Refonte des dossiers #1 #2

---
+++ Boutique Cegep Matane/menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title> Boutique du Cégep de Matane </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./Decoration/menu.css">
+</head>
+<body>
+	<header class="menu">
+    	<div class="logo-menu">    
+	        <a href="index.php"><img src="./Ressources/images/logo_cegep.png" alt="Logo Cégep Matane"/></a>	        
+    	</div>
+        <div class="menu-navigation">
+	         <a href="index.php" class="bouton-menu"> Accueil </a>
+	         <a href="magasiner.php" class="bouton-menu"> Magasiner </a>
+	         <a href="administration.php" class="bouton-menu"> Administration </a>
+	         <a href="apropos.php" class="bouton-menu"> À Propos de nous </a>
+        </div>
+  	</header>
+</body>
