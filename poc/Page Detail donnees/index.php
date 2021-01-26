aed2a1d4ddc6a9cf2d43279d3eecea92e7d2eb8a
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2021-01-26 10:31:09 -0500

    refonte des dossiers

--- poc/Page Detail donnees/index.php
+++ poc/Page Detail donnees/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<title> Boutique du Cégep de Matane </title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="./Decoration/style.css">
 </head>
 <body>
 	<header class="menu">
