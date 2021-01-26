11d671bf28844e82fdbb8702e289043282b2c382
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2021-01-26 11:06:43 -0500

    Implémentation du menu sans navigation #4
    implémentation du menu sans navigation

---
+++ poc/Page Menu/menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title> Boutique du Cégep de Matane </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./Decoration/menu.css">
+</head>
+<body>
+	<header class="menu">
+    	<div class="logo-menu">    
+	        <img src="./Decoration/logo_cegep.png" alt="Logo Cégep Matane"/>	        
+    	</div>
+        <div class="menu-navigation">
+	         <a href="#" class="bouton-menu"> Accueil </a>
+	         <a href="#" class="bouton-menu"> Magasiner </a>
+	         <a href="#" class="bouton-menu"> Panneau Administration </a>
+	         <a href="#" class="bouton-menu"> À Propos de nous </a>
+        </div>
+  	</header>
+</body>
