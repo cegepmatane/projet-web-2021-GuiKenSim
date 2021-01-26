b32c52c8d6734c8f3b12fe97afe34ecd2702fa31
guillaumedalbignac <dalbignacguillaume@gmail.com>
2021-01-26 11:06:17 -0500

    Refonte des dossiers page accueil et à propos#1
    Réorganisations des dossiers poc. Suppression des dossiers sans css

--- poc/Page Accueil/index.php
+++ poc/Page Accueil/index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<title> Boutique du Cégep de Matane </title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="./Decoration/accueil.css">
 </head>
 
 <body>
-
 	<header class="menu">
     	<div class="logo">    
-	        <a href="index.php"><img src="./logo_cegep.png" alt="Logo Cégep Matane" class="logo" /></a>      
+	        <a href="index.php"><img src="./ressources/images/logo_cegep.png" alt="Logo Cégep Matane" class="logo" /></a>      
     	</div>
     	<div class="lien_administration">
 	         <a href="administration.php"> Administration </a>
@@ -31,7 +30,6 @@
   			<p> © Tous droits réservés - Site développé par Kenny Maréchal, Simon Delarue et Guillaume d'Albignac </p>
   		</div>  		
   	</footer>
-
 </body>
 </html>
 
