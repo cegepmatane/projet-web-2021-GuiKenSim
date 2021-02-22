d74a33a6b9aa7a65aad8925fd7e31ab682bb079c
Simon Delarue <51883276+simondelarue14@users.noreply.github.com>
2021-02-22 15:39:41 -0500

    #19 Ajout du CSS, du code pour changer de pseudo et mail
    TODO : changement de mot de passe + changement du nom d'utilisateur dans la table de l'historique de transaction

--- BoutiqueCegepMatane-version-1/accesseur/UtilisateurDAO.php
+++ BoutiqueCegepMatane-version-1/accesseur/UtilisateurDAO.php
@@ -63,46 +63,54 @@ class UtilisateurDAO{
 
         
     }
-    public static function modifierUtilisateur($utilisateur,$pseudo){
+    public static function modifierUtilisateur($utilisateur,$changementsUtilisateur){
         
-        //echo "pseudo : ".$utilisateur->getPseudo();
-        $MESSAGE_SQL_PSEUDO_EXISTANT = "UPDATE utilisateurs SET pseudo= :pseudo,courriel= :courriel, WHERE pseudo = :ancienPseudo";
-        $baseDeDonnees = BaseDeDonnees::getConnexion();
-        $requetteChercherUtilisateur = $baseDeDonnees->prepare($MESSAGE_SQL_PSEUDO_EXISTANT);
-        $pseudoUtilisateur = $utilisateur->getPseudo();
-        $requetteChercherUtilisateur->bindParam(':pseudo', $pseudoUtilisateur, PDO::PARAM_STR);
-        $requetteChercherUtilisateur->execute();
-        $utilisateurExistant = $requetteChercherUtilisateur->fetch(PDO::FETCH_ASSOC);
+        
+        $ancienPseudo = $utilisateur->getPseudo();
+        $nouveauPseudo = $changementsUtilisateur->getPseudo();
+        $ancienCourriel = $utilisateur->getCourriel();
+        $nouveauCourriel = $changementsUtilisateur->getCourriel();
+     
+        // fonction pour vérifier que qu'un autre utilisateur n'a pas déjà le pseudo choisi
+        if($ancienPseudo != $nouveauPseudo){
+            $MESSAGE_SQL_PSEUDO_EXISTANT = "SELECT pseudo FROM utilisateurs WHERE pseudo = :pseudo";
+            $baseDeDonnees = BaseDeDonnees::getConnexion();
+            $requetteChercherUtilisateur = $baseDeDonnees->prepare($MESSAGE_SQL_PSEUDO_EXISTANT);
+            $requetteChercherUtilisateur->bindParam(':pseudo', $nouveauPseudo, PDO::PARAM_STR);
+            $requetteChercherUtilisateur->execute();
+            $utilisateurExistant = $requetteChercherUtilisateur->fetch(PDO::FETCH_ASSOC);
 
-        if (isset($utilisateurExistant) && !empty($utilisateurExistant)){
-        return "utilisateur ou pseudo déjà existant";
+            if (isset($utilisateurExistant) && !empty($utilisateurExistant)){
+                return "Le pseudo choisi est déjà pris";
+            }
+
+        }
+        // fonction pour vérifier que qu'un autre utilisateur n'a pas déjà le courriel choisi
+        if($ancienCourriel != $nouveauCourriel){
+            $MESSAGE_SQL_COURRIEL_EXISTANT = "SELECT courriel FROM utilisateurs WHERE courriel = :courriel";
+            $baseDeDonnees = BaseDeDonnees::getConnexion();
+            $requetteChercherCourriel = $baseDeDonnees->prepare($MESSAGE_SQL_COURRIEL_EXISTANT);
+            $requetteChercherCourriel->bindParam(':courriel', $nouveauCourriel, PDO::PARAM_STR);
+            $requetteChercherCourriel->execute();
+            $courrielExistant = $requetteChercherCourriel->fetch(PDO::FETCH_ASSOC);
+
+            if (isset($courrielExistant) && !empty($courrielExistant)){
+            return "Le courriel choisi est déjà pris";
+            }
         }
         
-        $MESSAGE_SQL_COURRIEL_EXISTANT = "SELECT courriel FROM utilisateurs WHERE courriel = :courriel";
+        
+        // Création de la requête et bind param des arguments puis exécution
+        //UPDATE utilisateurs SET pseudo= 'simone',courriel= 'simon.delarue2@gmail.com' WHERE pseudo = 'simon';
+        $MESSAGE_SQL_MODIFIER_UTILISATEUR = "UPDATE utilisateurs SET pseudo = :pseudo , courriel = :courriel WHERE pseudo = :ancienPseudo";
         $baseDeDonnees = BaseDeDonnees::getConnexion();
-        $requetteChercherCourriel = $baseDeDonnees->prepare($MESSAGE_SQL_COURRIEL_EXISTANT);
-        $courrielUtilisateur = $utilisateur->getCourriel();
-        $requetteChercherCourriel->bindParam(':courriel', $courrielUtilisateur, PDO::PARAM_STR);
-        $requetteChercherCourriel->execute();
-        $courrielExistant = $requetteChercherCourriel->fetch(PDO::FETCH_ASSOC);
-
-        if (isset($courrielExistant) && !empty($courrielExistant)){
-        return "courriel déjà existant";
-        }
+        $requetteModifierUtilisateur = $baseDeDonnees->prepare($MESSAGE_SQL_MODIFIER_UTILISATEUR);
+        $requetteModifierUtilisateur->bindParam(':pseudo', $nouveauPseudo, PDO::PARAM_STR);
+        $requetteModifierUtilisateur->bindParam(':courriel', $nouveauCourriel, PDO::PARAM_STR);
+        $requetteModifierUtilisateur->bindParam(':ancienPseudo', $ancienPseudo, PDO::PARAM_STR);
+        $requetteModifierUtilisateur->execute();
         
-        
-        
-        $MESSAGE_SQL_AJOUTER_UTILISATEUR = "INSERT INTO utilisateurs (pseudo, courriel, motdepasse) VALUES (".":pseudo".",".":courriel".",".":motdepasse".");";
-        $baseDeDonnees = BaseDeDonnees::getConnexion();
-        $pseudo = $utilisateur->getPseudo();
-        $courriel = $utilisateur->getCourriel();
-        $motdepasse = $utilisateur->getMotDePasse();
-        $requetteAjouterUtilisateur= $baseDeDonnees->prepare($MESSAGE_SQL_AJOUTER_UTILISATEUR);
-        $requetteAjouterUtilisateur->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
-        $requetteAjouterUtilisateur->bindParam(':courriel', $courriel, PDO::PARAM_STR);
-        $requetteAjouterUtilisateur->bindParam(':motdepasse', $motdepasse, PDO::PARAM_STR);
-        $requetteAjouterUtilisateur->execute();
-        $req = pg_get_result($requetteAjouterUtilisateur);
+        $req = pg_get_result($requetteModifierUtilisateur);
         echo pg_result_error($req);
         return false;
 
--- BoutiqueCegepMatane-version-1/profil.php
+++ BoutiqueCegepMatane-version-1/profil.php
@@ -1,14 +1,96 @@
 <?php
     require_once "./modele/Utilisateur.php";
+    require "./accesseur/UtilisateurDAO.php";  
     session_start();
     if(isset($_POST['deconnexion']))
     {
         session_destroy();
         header('location:index.php');
     }
-    $sessionUtilisateur = $_SESSION["utilisateur"];
-$succes_pseudo_courriel = "erreur l'utilisateur existe déjà";
-$succes_motdepasse = "erreur les mots de passe ne correspondent pas";
+    $sessionUtilisateur = new Utilisateur($_SESSION["utilisateur"]->getId(),$_SESSION["utilisateur"]->getPseudo(),$_SESSION["utilisateur"]->getCourriel(),$_SESSION["utilisateur"]->getMotDePasse());
+    $succes_pseudo_courriel = "";
+    $succes_motdepasse = "erreur les mots de passe ne correspondent pas";
+
+
+
+    if(isset($_POST['BoutonChangementNomCourriel'])){ //check if form was submitted
+        
+        // récupération des variables + les mettres dans un objet utilisateurs
+        $pseudo = $_REQUEST['pseudo'];
+        $courriel = $_REQUEST['courriel'];
+        $motdepasse = $_REQUEST['motdepasse'];
+        $changementsUtilisateur = new Utilisateur(null,$pseudo,$courriel,$motdepasse);
+        $erreurs= array(); // tableau ou on stocke les erreurs
+        // vérification que le mot de passe est bon
+        
+        if (!password_verify($changementsUtilisateur->getMotDePasse(), $sessionUtilisateur->getMotDePasse())) {
+            $erreurs[]= "Le mot de passe est incorrect";
+        }
+        // vérification des entrées utilisateur
+        
+            
+        if (!preg_match("/^[a-zA-Z-0-9-' ]*$/",$changementsUtilisateur->getPseudo())) {
+            $erreurs[]="Seulement des lettres, chiffres et espaces sont autorisés";
+        }
+
+        if(empty($changementsUtilisateur->getPseudo())==true || empty($changementsUtilisateur->getCourriel())==true) {
+            $erreurs[]="Un des champs obligatoire est vide !";
+        }
+        if (!filter_var($changementsUtilisateur->getCourriel(), FILTER_VALIDATE_EMAIL)) {
+            $erreurs[]="Veuillez entrer un courriel valide";
+        }
+        if(!is_string($changementsUtilisateur->getPseudo())){
+            $erreurs[]="Le pseudo doit être du texte !";
+        }
+        if(!is_string($changementsUtilisateur->getCourriel())){
+            $erreurs[]="Le courriel doit être du texte !";
+        }
+        if(is_link($changementsUtilisateur->getPseudo()) || is_link($changementsUtilisateur->getCourriel())){
+            $erreurs[]="Il ne faut pas mettre de liens dans les champs";
+        }
+            
+        
+        
+        
+        // s'il n'y a pas d'erreur avant l'envoi
+        if(empty($erreurs)==true) {
+            
+            
+            $erreurs[]= UtilisateurDAO::modifierUtilisateur($sessionUtilisateur,$changementsUtilisateur); // on exécute la fonction ajouterUtilisateur
+            
+            // On vérifie qu'il n'y a pas d'erreurs
+            if($erreurs[0] == false){
+                $succes_pseudo_courriel = "succès";
+                $_SESSION["utilisateur"]->setPseudo($changementsUtilisateur->getPseudo());
+                $_SESSION["utilisateur"]->setCourriel($changementsUtilisateur->getCourriel());
+                header('location:profil.php');
+            }else{
+                // foreach pour afficher toutes les erreurs venant de la base
+                foreach ($erreurs as $erreur){
+                    $succes_pseudo_courriel = $succes_pseudo_courriel."● erreur : ".$erreur."<br/>";
+                }
+            }
+            
+        }else{
+            foreach ($erreurs as $erreur){
+                    $succes_pseudo_courriel = $succes_pseudo_courriel."● erreur : ".$erreur."<br/>";
+                    echo "erreur : ".$erreur;
+                }
+        }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +114,7 @@ $succes_motdepasse = "erreur les mots de passe ne correspondent pas";
                     type="text"
                     name="pseudo"
                     autocomplete="off" 
-                    value=<?php echo $sessionUtilisateur->getPseudo(); ?>
+                    value="<?php echo $sessionUtilisateur->getPseudo();?>"
                     class="page-profil-formulaire-input"
                     title="Nom d'utilisateur"
                     required=true
@@ -44,7 +126,7 @@ $succes_motdepasse = "erreur les mots de passe ne correspondent pas";
                 name="courriel"
                 type = "text"
                 autocomplete="off" 
-                value=<?php echo $sessionUtilisateur->getCourriel(); ?>
+                value="<?php echo $sessionUtilisateur->getCourriel(); ?>"
                 class="page-profil-formulaire-input"
                 title="Adresse courriel"
                 required=true
@@ -62,7 +144,10 @@ $succes_motdepasse = "erreur les mots de passe ne correspondent pas";
                 <p class="page-profil-msgIncorrect"><?php echo $succes_pseudo_courriel ?></p>
                 <input type="submit" value="Modifier" class="page-profil-bouton" name="BoutonChangementNomCourriel"/>
             </form>
-        </div>  
+        </div>
+        
+        
+        
         <div class="boite-decoration">
             <form action="" class="page-profil-formulaire" method="post">
                 <h2>Changer le mot de passe</h2>
