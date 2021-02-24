5d449eb9ab9eb3047b98d10f93cf21c6bfef7ec8
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2021-02-24 04:26:57 +0400

    Update BaseDeDonnees.php

--- BoutiqueCegepMatane-version-web/administration/accesseur/BaseDeDonnees.php
+++ BoutiqueCegepMatane-version-web/administration/accesseur/BaseDeDonnees.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 class BaseDeDonnees{
   public static function getConnexion(){
     $usager = 'kenny';
-    $motdepasse = 'Toad781227@';
+    $motdepasse = '';
     $hote = 'localhost';
     $base = 'boutiquecegep';
     $dsn = "pgsql:host=$hote;dbname=$base;";
