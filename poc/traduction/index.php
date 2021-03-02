4309c28145dbd1da3bd52eff9c9e1c41d48a8ea0
guillaumedalbignac <dalbignacguillaume@gmail.com>
2021-03-02 16:48:01 -0500

    #24 Poc de traduction
    - 1er jet d'un système de traduction

--- poc/traduction/index.php
+++ poc/traduction/index.php
@@ -1,32 +1,19 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<title> Boutique du Cégep de Matane </title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="./Decoration/accueil.css">
-</head>
+<?php
+$locale = "en_CA.UTF-8"; // $locale = "en_CA";
 
-<body class="page_accueil">
-	<?php include 'menu.php' ?>
+$racine = "./locale";
+$domaine = "messages";
 
-  	<div class="accueil">
-  		<h1 class="titre_accueil"> Bienvenue sur la boutique en ligne du Cégep de Matane ! Cliquez sur MAGASINER pour commencer. </h1>
-  		<a href="magasiner.php" class="bouton_magasiner"> MAGASINER </a>
-	  		<div class="lien_apropos">
-	  			<a href="apropos.php"> À Propos de nous </a>
-	  		</div> 		
-  	</div> 
+putenv('LC_ALL='.$locale);
+setlocale(LC_ALL, $locale);
 
-  <?php include 'footer.php' ?>
-  	
-</body>
-</html>
+bindtextdomain($domaine, $racine);
+textdomain($domaine);
 
-
-
-
-
-
-
-
+$results = gettext("Bonjour");
+if ($results === "Bonjour") {
+    echo "Erreur de traduction \n";
+} else {
+    echo $results;
+}
+?>
